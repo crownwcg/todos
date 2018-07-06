c901f654f284954d9b9cb3a02f3fcf0381462498
Wang <chenwang@HANM15497-2.local>
2018-07-06 11:22:23 +0800

    create container

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import Header from '../components/Header'
+import List from '../components/List'
+import Footer from '../components/Footer'
+import '../style/App.css'
+
+class App extends Component {
+
+  	render() {
+
+  		const {
+  			items,
+  			input,
+  			handleChange,
+  			handleSubmit,
+  			handleClick,
+  			clear,
+  			all,
+  			active,
+  			completed
+  		} = this.props
+  		
+    	return (
+      		<div>
+		        <Header />
+				<form onSubmit={e => handleSubmit(e, input)}>
+					<input value={input}
+						   onChange={e => handleChange(e)}/>
+				</form>
+		        <List items={items}
+		        	  handleClick={handleClick}/>
+		        <span>
+		        	<button className="All" onClick={e => all(items)}>All</button>
+		        	<button className="Active" onClick={e => active(items)}>Active</button>
+		        	<button className="Completed" onClick={e => completed(items)}>Completed</button>
+		       		<button className="Clear completed" onClick={e => clear(items)}>Clear completed</button>
+		       	</span>
+		        <Footer />
+      		</div>
+    	)
+  	}
+}
+
+export default App
--- src/container/App.js
+++ src/container/App.js
@@ -1,9 +1,4 @@
-import React, { Component } from 'react'
-import Header from '../components/Header'
-import List from '../components/List'
-import Footer from '../components/Footer'
-import '../style/App.css'
-
+import App from '../components/App'
 import { connect } from 'react-redux'
 import { addItem, 
 		 clearItem, 
@@ -39,41 +34,5 @@ const mapDispatchToProps = dispatch => ({
 			dispatch(setInvisible(item.id))
 		),
 })
-
-class App extends Component {
-
-  	render() {
-
-  		const {
-  			items,
-  			input,
-  			handleChange,
-  			handleSubmit,
-  			handleClick,
-  			clear,
-  			all,
-  			active,
-  			completed
-  		} = this.props
-    	return (
-      		<div>
-		        <Header />
-				<form onSubmit={e => handleSubmit(e, input)}>
-					<input value={input}
-						   onChange={e => handleChange(e)}/>
-				</form>
-		        <List items={items}
-		        	  handleClick={handleClick}/>
-		        <span>
-		        	<button className="All" onClick={e => all(items)}>All</button>
-		        	<button className="Active" onClick={e => active(items)}>Active</button>
-		        	<button className="Completed" onClick={e => completed(items)}>Completed</button>
-		       		<button className="Clear completed" onClick={e => clear(items)}>Clear completed</button>
-		       	</span>
-		        <Footer />
-      		</div>
-    	)
-  	}
-}
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
